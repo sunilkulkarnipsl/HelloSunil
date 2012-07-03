5fa9201a1ad532b131d66ab941c5c8f40c4fff38
unknown <sunikulk@.cisco.com>
2012-07-03 17:11:24 +0530

    Hello program added for testing purpose

---
+++ MySampleGitProject/src/com/Hello.java
@@ -0,0 +1,17 @@
+package com;
+
+/**
+ * @author Sunil Kulkarni
+ * 
+ */
+public class Hello {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello Sunil...");
+
+  }
+
+}
